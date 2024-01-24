5a6d87ebe499199becd1f74f8f0d78f9f8e8307e
Artur Baldo Ramos <arturbaldoramos@gmail.com>
2024-01-24 14:09:38 -0300

    feat: get user method

--- pkg/router/routes.go
+++ pkg/router/routes.go
@@ -1,7 +1,33 @@
 package router
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"github.com/arturbaldoramos/go-authentication/pkg/models"
+	u "github.com/arturbaldoramos/go-authentication/pkg/utils"
+	"github.com/gofiber/fiber/v2"
+)
 
 func initializeRoutes(router *fiber.App) {
+	router.Get("/user", func(ctx *fiber.Ctx) error {
+		uuid := ctx.Query("uuid")
+		fmt.Printf("")
+		user := models.GetUser(uuid)
 
+		if user != nil {
+			resp := u.Message(true, "success")
+
+			userWithoutPassword := map[string]interface{}{
+				"id":        user.ID,
+				"name":      user.Name,
+				"email":     user.Email,
+				"CreatedAt": user.CreatedAt,
+				"UpdatedAt": user.UpdatedAt,
+				"DeletedAt": user.DeletedAt,
+			}
+
+			resp["user"] = userWithoutPassword
+			return ctx.JSON(resp)
+		}
+		return ctx.JSON(u.Message(false, "user not found"))
+	})
 }
