f7de685a131afa5928b7978386c570ae9afff858
Artur Baldo Ramos <arturbaldoramos@gmail.com>
2024-01-29 17:21:57 -0300

    feat: implement auth middleware

---
+++ pkg/middleware/middleware.go
@@ -0,0 +1,50 @@
+package middleware
+
+import (
+	"github.com/arturbaldoramos/go-authentication/pkg/utils"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt"
+	"os"
+)
+
+func AuthMiddleware(c *fiber.Ctx) error {
+	tokenString := c.Cookies("token")
+	if tokenString == "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(utils.Message(false, "Missing authentication token"))
+	}
+
+	// Parse JWT token
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Check token signing method
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, jwt.NewValidationError("invalid signing method", jwt.ValidationErrorSignatureInvalid)
+		}
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+	if err != nil {
+		if ve, ok := err.(*jwt.ValidationError); ok {
+			if ve.Errors&jwt.ValidationErrorMalformed != 0 {
+				return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+					"message": "Malformed token",
+				})
+			} else if ve.Errors&(jwt.ValidationErrorExpired|jwt.ValidationErrorNotValidYet) != 0 {
+				return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+					"message": "Token is expired or not yet valid",
+				})
+			} else {
+				return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+					"message": "Couldn't handle this token",
+				})
+			}
+		}
+	}
+
+	// Check if token is valid
+	if token.Valid {
+		// Continue chain
+		return c.Next()
+	}
+
+	// Continue chain
+	return c.Next()
+}
--- pkg/router/routes.go
+++ pkg/router/routes.go
@@ -2,14 +2,15 @@ package router
 
 import (
 	"github.com/arturbaldoramos/go-authentication/pkg/handler"
+	"github.com/arturbaldoramos/go-authentication/pkg/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
 func initializeRoutes(router *fiber.App) {
-	router.Get("/user/:uuid", handler.GetUserById)
-	router.Delete("/user/:uuid", handler.DeleteUserById)
-	router.Get("/user", handler.GetAllUsers)
-	router.Post("/user", handler.CreateUser)
+	router.Get("/user/:uuid", middleware.AuthMiddleware, handler.GetUserById)
+	router.Delete("/user/:uuid", middleware.AuthMiddleware, handler.DeleteUserById)
+	router.Get("/user", middleware.AuthMiddleware, handler.GetAllUsers)
+	router.Post("/user", middleware.AuthMiddleware, handler.CreateUser)
 
 	router.Post("/login", handler.Login)
 	router.Post("/logout", handler.Logout)
