aa9933e78d141f9a137173d48d6811c3df529a4e
Artur Baldo Ramos <arturbaldoramos@gmail.com>
2024-02-06 15:10:28 -0300

    feat: render method

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,5 +1,19 @@
 package utils
 
+import (
+	"github.com/a-h/templ"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
+)
+
 func Message(status bool, message string) map[string]interface{} {
 	return map[string]interface{}{"status": status, "message": message}
+}
+
+func Render(c *fiber.Ctx, component templ.Component, options ...func(*templ.ComponentHandler)) error {
+	componentHandler := templ.Handler(component)
+	for _, o := range options {
+		o(componentHandler)
+	}
+	return adaptor.HTTPHandler(componentHandler)(c)
 }
